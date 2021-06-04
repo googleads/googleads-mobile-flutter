99170898397e0f32f5188a8431814f91ce257fb0
Maurice Parrish <bparr2450@gmail.com>
2021-06-04 12:35:38 -0700

    support for more classes

--- packages/google_mobile_ads/android/src/main/java/io/flutter/plugins/googlemobileads/adcontainers/AdContainersChannelRegistrar.java
+++ packages/google_mobile_ads/android/src/main/java/io/flutter/plugins/googlemobileads/adcontainers/AdContainersChannelRegistrar.java
@@ -1,5 +1,7 @@
 package io.flutter.plugins.googlemobileads.adcontainers;
 
+import android.content.Context;
+
 import github.penguin.reference.reference.TypeChannelMessenger;
 
 public class AdContainersChannelRegistrar extends AdContainersChannelLibrary.$ChannelRegistrar {
@@ -8,13 +10,26 @@ public class AdContainersChannelRegistrar extends AdContainersChannelLibrary.$Ch
   }
 
   public static class AdContainersLibraryImplementations extends AdContainersChannelLibrary.$LibraryImplementations {
-    public AdContainersLibraryImplementations(TypeChannelMessenger messenger) {
+    public final Context context;
+
+    public AdContainersLibraryImplementations(TypeChannelMessenger messenger, Context context) {
       super(messenger);
+      this.context = context;
     }
 
     @Override
     public AdRequestHandler getHandlerAdRequest() {
       return new AdRequestHandler();
+    }
+
+    @Override
+    public AdManagerAdRequestHandler getHandlerAdManagerAdRequest() {
+      return new AdManagerAdRequestHandler();
+    }
+
+    @Override
+    public AdSizeHandler getHandlerAdSize() {
+      return new AdSizeHandler(this);
     }
   }
 
@@ -22,6 +37,36 @@ public class AdContainersChannelRegistrar extends AdContainersChannelLibrary.$Ch
     @Override
     public AdRequestProxy $$create(TypeChannelMessenger messenger) {
       return new AdRequestProxy();
+    }
+  }
+
+  public static class AdManagerAdRequestHandler extends AdContainersChannelLibrary.$AdManagerAdRequestHandler {
+    @Override
+    public AdManagerAdRequestProxy $$create(TypeChannelMessenger messenger) {
+      return new AdManagerAdRequestProxy();
+    }
+  }
+
+  public static class AdSizeHandler extends AdContainersChannelLibrary.$AdSizeHandler {
+    public final AdContainersLibraryImplementations implementations;
+
+    public AdSizeHandler(AdContainersLibraryImplementations implementations) {
+      this.implementations = implementations;
+    }
+
+    @Override
+    public AdSizeProxy $$create(TypeChannelMessenger messenger, Integer width, Integer height, String constant) {
+      return new AdSizeProxy(width, height, constant);
+    }
+
+    @Override
+    public AdSizeProxy $getLandscapeAnchoredAdaptiveBannerAdSize(TypeChannelMessenger messenger, Integer width) {
+      return AdSizeProxy.getLandscapeAnchoredAdaptiveBannerAdSize(width, implementations);
+    }
+
+    @Override
+    public AdSizeProxy $getPortraitAnchoredAdaptiveBannerAdSize(TypeChannelMessenger messenger, Integer width) {
+      return AdSizeProxy.getPortraitAnchoredAdaptiveBannerAdSize(width, implementations);
     }
   }
 }
---
+++ packages/google_mobile_ads/android/src/main/java/io/flutter/plugins/googlemobileads/adcontainers/AdSizeProxy.java
@@ -0,0 +1,48 @@
+package io.flutter.plugins.googlemobileads.adcontainers;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.ads.AdSize;
+
+public class AdSizeProxy implements AdContainersChannelLibrary.$AdSize {
+  public final AdSize adSize;
+
+  public static AdSizeProxy getPortraitAnchoredAdaptiveBannerAdSize(Integer width, AdContainersChannelRegistrar.AdContainersLibraryImplementations implementations) {
+    return new AdSizeProxy(AdSize.getPortraitAnchoredAdaptiveBannerAdSize(implementations.context, width), implementations);
+  }
+
+  public static AdSizeProxy getLandscapeAnchoredAdaptiveBannerAdSize(Integer width, AdContainersChannelRegistrar.AdContainersLibraryImplementations implementations) {
+    return new AdSizeProxy(AdSize.getLandscapeAnchoredAdaptiveBannerAdSize(implementations.context, width), implementations);
+  }
+
+  private static AdSize getAdSizeConstant(String constant) {
+    switch (constant) {
+      case "banner":
+        return AdSize.BANNER;
+      case "largeBanner":
+        return AdSize.LARGE_BANNER;
+      case "mediumRectangle":
+        return AdSize.MEDIUM_RECTANGLE;
+      case "fullBanner":
+        return AdSize.FULL_BANNER;
+      case "leaderboard":
+        return AdSize.LEADERBOARD;
+      case "smartBannerPortrait":
+      case "smartBannerLandscape":
+        return AdSize.SMART_BANNER;
+    }
+
+    throw new IllegalArgumentException("No constant found for value: " + constant);
+  }
+  
+  public AdSizeProxy(Integer width, Integer height, String constant) {
+    this(constant != null ? getAdSizeConstant(constant) : new AdSize(width, height), null);
+  }
+
+  public AdSizeProxy(AdSize adSize, @Nullable AdContainersChannelRegistrar.AdContainersLibraryImplementations implementations) {
+    this.adSize = adSize;
+    if (implementations != null) {
+      implementations.getChannelAdSize().$$create(this, false, adSize.getWidth(), adSize.getHeight(), null);
+    }
+  }
+}
