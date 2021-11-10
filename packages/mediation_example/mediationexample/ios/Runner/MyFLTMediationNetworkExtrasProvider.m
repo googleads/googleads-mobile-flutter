3973095c253b86d12f265e84541bf3946db4a0ec
jjliu15 <jamesjliu@google.com>
2021-11-09 16:10:18 -0800

    Add license files and headers.

--- packages/mediation_example/mediationexample/ios/Runner/MyFLTMediationNetworkExtrasProvider.m
+++ packages/mediation_example/mediationexample/ios/Runner/MyFLTMediationNetworkExtrasProvider.m
@@ -1,9 +1,16 @@
+// Copyright 2021 Google LLC
 //
-//  MyFLTMediationNetworkExtrasProvider.m
-//  Runner
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by James Liu on 10/18/21.
+// https://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import "MyFLTMediationNetworkExtrasProvider.h"
 #import <AppLovinAdapter/GADMAdapterAppLovinExtras.h>
