712a8c2f469a818cdacba9b5b469984befc35099
jjliu15 <jamesjliu@google.com>
2022-11-10 12:32:04 -0800

    Remove unnecessary github steps

--- .github/workflows/scripts/build-example.sh
+++ .github/workflows/scripts/build-example.sh
@@ -32,9 +32,6 @@ fi
 
 if [ "$ACTION" == "ios" ]
 then
-  flutter clean;
-  flutter pub get;
-  pod install;
   melos exec -c 1 --scope="$GOOGLEMOBILEADS_PLUGIN_SCOPE_EXAMPLE" -- \
     flutter build ios --no-codesign --simulator --debug --target="$TARGET_FILE" --dart-define=CI=true
   exit
