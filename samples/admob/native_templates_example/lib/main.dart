10e509a068061a4425fff4c47c2aa846f6cd310f
Justin Malandruccolo <jmalandruccolo@google.com>
2023-01-26 17:24:57 -0500

    Setting up templates ui code

--- samples/admob/native_templates_example/lib/main.dart
+++ samples/admob/native_templates_example/lib/main.dart
@@ -27,6 +27,13 @@ class NativeExampleState extends State<NativeExample> {
       : 'ca-app-pub-3940256099942544/3986624511';
 
   @override
+  void initState() {
+    super.initState();
+
+    _loadAd();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Native Example',
@@ -41,10 +48,56 @@ class NativeExampleState extends State<NativeExample> {
   }
 
   /// Loads a native ad.
-  void _loadAd() {}
+  void _loadAd() {
+    _nativeAd = NativeAd(
+        adUnitId: _adUnitId,
+        listener: NativeAdListener(
+          onAdLoaded: (ad) {
+            // ignore: avoid_print
+            print('$NativeAd loaded.');
+          },
+          onAdFailedToLoad: (ad, error) {
+            // ignore: avoid_print
+            print('$NativeAd failedToLoad: $error');
+            ad.dispose();
+          },
+          onAdClicked: (ad) {},
+          onAdImpression: (ad) {},
+          onAdClosed: (ad) {},
+          onAdOpened: (ad) {},
+          onAdWillDismissScreen: (ad) {},
+          onPaidEvent: (ad, valueMicros, precision, currencyCode) {},
+        ),
+        request: const AdRequest(),
+        nativeTemplateStyle: NativeTemplateStyle(
+            templateType: TemplateType.medium,
+            mainBackgroundColor: Colors.purple,
+            callToActionTextStyle: NativeTemplateTextStyle(
+                textColor: Colors.cyan,
+                backgroundColor: Colors.red,
+                style: NativeTemplateFontStyle.monospace,
+                size: 16.0),
+            primaryTextStyle: NativeTemplateTextStyle(
+                textColor: Colors.red,
+                backgroundColor: Colors.cyan,
+                style: NativeTemplateFontStyle.italic,
+                size: 16.0),
+            secondaryTextStyle: NativeTemplateTextStyle(
+                textColor: Colors.green,
+                backgroundColor: Colors.black,
+                style: NativeTemplateFontStyle.bold,
+                size: 16.0),
+            tertiaryTextStyle: NativeTemplateTextStyle(
+                textColor: Colors.brown,
+                backgroundColor: Colors.amber,
+                style: NativeTemplateFontStyle.normal,
+                size: 16.0)))
+      ..load();
+  }
 
   @override
   void dispose() {
+    _nativeAd?.dispose();
     super.dispose();
   }
 }
