9cd855e04ddd7bd9f0369b83f363dc056a57e3c5
jjliu15 <58007281+jjliu15@users.noreply.github.com>
2023-03-15 11:03:22 -0700

    Do not use melos in build-example (#797)

--- .github/workflows/scripts/build-example.sh
+++ .github/workflows/scripts/build-example.sh
@@ -12,34 +12,21 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-DEFAULT_TARGET="./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart"
+DEFAULT_TARGET="lib/main.dart"
 
 ACTION=$1
 TARGET_FILE=${2:-$DEFAULT_TARGET}
+EXAMPLE_APP_DIRECTORY=$3
 
-melos bootstrap
+cd "$EXAMPLE_APP_DIRECTORY"
 
 if [ "$ACTION" == "android" ]
 then
-  melos exec -c 1 --scope="$GOOGLEMOBILEADS_PLUGIN_SCOPE_EXAMPLE" -- \
-    flutter build apk --debug --target="$TARGET_FILE" --dart-define=CI=true
-  MELOS_EXIT_CODE=$?
-  pkill dart || true
-  pkill java || true
-  pkill -f '.*GradleDaemon.*' || true
-  exit $MELOS_EXIT_CODE
+  flutter build apk --debug --target="$TARGET_FILE" --dart-define=CI=true
 fi
 
 if [ "$ACTION" == "ios" ]
 then
-  melos exec -c 1 --scope="$GOOGLEMOBILEADS_PLUGIN_SCOPE_EXAMPLE" -- \
-    flutter build ios --no-codesign --simulator --debug --target="$TARGET_FILE" --dart-define=CI=true
-  exit
+  flutter build ios --no-codesign --simulator --debug --target="$TARGET_FILE" --dart-define=CI=true
 fi
 
-if [ "$ACTION" == "macos" ]
-then
-  melos exec -c 1 --scope="$GOOGLEMOBILEADS_PLUGIN_SCOPE_EXAMPLE" -- \
-    flutter build macos --debug --target="$TARGET_FILE" --device-id=macos --dart-define=CI=true
-  exit
-fi
