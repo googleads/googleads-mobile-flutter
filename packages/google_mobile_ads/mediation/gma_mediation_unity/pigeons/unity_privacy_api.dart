c7a4eb5ab498c4ac4a3f7afe7a2e6c72d72e4d4b
Aldo Becerril <aldobecerril@google.com>
2024-02-22 11:30:00 -0800

    Format fix. GitHub workflows created and updated for Mediation folder

--- packages/google_mobile_ads/mediation/gma_mediation_unity/pigeons/unity_privacy_api.dart
+++ packages/google_mobile_ads/mediation/gma_mediation_unity/pigeons/unity_privacy_api.dart
@@ -18,8 +18,10 @@ import 'package:pigeon/pigeon.dart';
   dartOut: 'lib/unity_privacy_api.g.dart',
   dartOptions: DartOptions(),
   kotlinOut:
-  'android/src/main/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_unity/UnityPrivacyApi.g.kt',
-  kotlinOptions: KotlinOptions(package: "io.flutter.plugins.googlemobileads.mediation.gma_mediation_unity"),
+      'android/src/main/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_unity/UnityPrivacyApi.g.kt',
+  kotlinOptions: KotlinOptions(
+      package:
+          "io.flutter.plugins.googlemobileads.mediation.gma_mediation_unity"),
   swiftOut: 'ios/Classes/UnityPrivacyApi.g.swift',
   swiftOptions: SwiftOptions(),
   dartPackageName: 'gma_mediation_unity',
