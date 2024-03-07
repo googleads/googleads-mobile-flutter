7f1e2fee67d5d858e8b77af329515a926355b56c
Aldo Becerril <aldobecerril@google.com>
2024-03-07 15:57:32 -0800

    Update GmaMediationMetaPluginTest.kt

--- packages/google_mobile_ads/mediation/gma_mediation_meta/android/src/test/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_meta/GmaMediationMetaPluginTest.kt
+++ packages/google_mobile_ads/mediation/gma_mediation_meta/android/src/test/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_meta/GmaMediationMetaPluginTest.kt
@@ -1,17 +1,23 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package io.flutter.plugins.googlemobileads.mediation.gma_mediation_meta
 
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import kotlin.test.Test
 import org.mockito.Mockito
-
-/*
- * This demonstrates a simple unit test of the Kotlin portion of this plugin's implementation.
- *
- * Once you have built the plugin's example app, you can run these tests from the command
- * line by running `./gradlew testDebugUnitTest` in the `example/android/` directory, or
- * you can run them directly from IDEs that support JUnit such as Android Studio.
- */
 
 internal class GmaMediationMetaPluginTest {
   @Test
