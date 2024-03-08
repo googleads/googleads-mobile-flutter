87f483ff8c439a1f490c16ca896a2ef47480e9a4
Aldo Becerril <aldobecerril@google.com>
2024-03-07 16:02:10 -0800

    Update GmaMediationUnityPluginTest.kt

--- packages/google_mobile_ads/mediation/gma_mediation_unity/android/src/test/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_unity/GmaMediationUnityPluginTest.kt
+++ packages/google_mobile_ads/mediation/gma_mediation_unity/android/src/test/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_unity/GmaMediationUnityPluginTest.kt
@@ -30,13 +30,6 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
-/*
- * This demonstrates a simple unit test of the Kotlin portion of this plugin's implementation.
- *
- * Once you have built the plugin's example app, you can run these tests from the command
- * line by running `./gradlew testDebugUnitTest` in the `example/android/` directory, or
- * you can run them directly from IDEs that support JUnit such as Android Studio.
- */
 @RunWith(AndroidJUnit4::class)
 internal class GmaMediationUnityPluginTest {
   private val context = ApplicationProvider.getApplicationContext<Context>()
