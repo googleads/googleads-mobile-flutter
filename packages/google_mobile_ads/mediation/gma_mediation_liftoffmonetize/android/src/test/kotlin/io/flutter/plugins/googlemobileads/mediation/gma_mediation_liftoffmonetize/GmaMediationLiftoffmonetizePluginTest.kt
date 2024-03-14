2606f424a73495d1a80ff3392ca647c0048838cf
Aldo Becerril <aldobecerril@google.com>
2024-03-14 10:44:44 -0700

    Added Android unit tests

--- packages/google_mobile_ads/mediation/gma_mediation_liftoffmonetize/android/src/test/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_liftoffmonetize/GmaMediationLiftoffmonetizePluginTest.kt
+++ packages/google_mobile_ads/mediation/gma_mediation_liftoffmonetize/android/src/test/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_liftoffmonetize/GmaMediationLiftoffmonetizePluginTest.kt
@@ -1,27 +1,54 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package io.flutter.plugins.googlemobileads.mediation.gma_mediation_liftoffmonetize
 
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import kotlin.test.Test
-import org.mockito.Mockito
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.vungle.ads.VunglePrivacySettings
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mockStatic
 
-/*
- * This demonstrates a simple unit test of the Kotlin portion of this plugin's implementation.
- *
- * Once you have built the plugin's example app, you can run these tests from the command
- * line by running `./gradlew testDebugUnitTest` in the `example/android/` directory, or
- * you can run them directly from IDEs that support JUnit such as Android Studio.
- */
-
+@RunWith(AndroidJUnit4::class)
 internal class GmaMediationLiftoffmonetizePluginTest {
   @Test
-  fun onMethodCall_getPlatformVersion_returnsExpectedValue() {
+  fun setGDPRStatus_withTruValue_invokesLiftoffSetGDPRStatus() {
     val plugin = GmaMediationLiftoffmonetizePlugin()
+    mockStatic(VunglePrivacySettings::class.java).use { mockVunglePrivacySettings ->
 
-    val call = MethodCall("getPlatformVersion", null)
-    val mockResult: MethodChannel.Result = Mockito.mock(MethodChannel.Result::class.java)
-    plugin.onMethodCall(call, mockResult)
+      plugin.setGDPRStatus(optedIn= true, consentMessageVersion= TEST_CONSENT_STRING)
 
-    Mockito.verify(mockResult).success("Android " + android.os.Build.VERSION.RELEASE)
+      mockVunglePrivacySettings.verify {
+        VunglePrivacySettings.setGDPRStatus(true, TEST_CONSENT_STRING)
+      }
+    }
+  }
+
+  @Test
+  fun setCCPAStatus_withTruValue_invokesLiftoffSetCCPAStatus() {
+    val plugin = GmaMediationLiftoffmonetizePlugin()
+    mockStatic(VunglePrivacySettings::class.java).use { mockVunglePrivacySettings ->
+
+      plugin.setCCPAStatus(optedIn= true)
+
+      mockVunglePrivacySettings.verify {
+        VunglePrivacySettings.setCCPAStatus(true)
+      }
+    }
+  }
+
+  companion object {
+    private const val TEST_CONSENT_STRING = "testConsentString"
   }
 }
--- packages/google_mobile_ads/mediation/gma_mediation_liftoffmonetize/example/lib/main.dart
+++ packages/google_mobile_ads/mediation/gma_mediation_liftoffmonetize/example/lib/main.dart
@@ -16,35 +16,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
   final _gmaMediationLiftoffmonetizePlugin = GmaMediationLiftoffmonetize();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _gmaMediationLiftoffmonetizePlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -54,8 +30,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: const Center(
+          child: Text('Test app'),
         ),
       ),
     );
