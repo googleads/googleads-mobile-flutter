70dd1caeb2e77e93a60c79bc7b6fd5fe5c302a56
Aldo Becerril <aldobecerril@google.com>
2024-04-11 15:43:56 -0700

    Added DT Exchange Adapter

---
+++ packages/mediation/gma_mediation_dtexchange/android/src/main/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_dtexchange/GmaMediationDTExchangePlugin.kt
@@ -0,0 +1,10 @@
+package io.flutter.plugins.googlemobileads.mediation.gma_mediation_dtexchange
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+/** Class that serves as bridge to get the adapter android dependency and make it available to a Flutter app. */
+class GmaMediationDTExchangePlugin: FlutterPlugin {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {}
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+}
