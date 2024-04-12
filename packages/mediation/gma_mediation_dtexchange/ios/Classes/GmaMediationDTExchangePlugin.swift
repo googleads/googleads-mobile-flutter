419eb1bd52a0c8deb6dd08f2f833d2f64e96d92e
Aldo Becerril <aldobecerril@google.com>
2024-04-12 15:44:47 -0700

    Added DT Exchange Adapter (#1052)
    * Added DT Exchange Adapter

---
+++ packages/mediation/gma_mediation_dtexchange/ios/Classes/GmaMediationDTExchangePlugin.swift
@@ -0,0 +1,7 @@
+import Flutter
+import UIKit
+
+/// Class that serves as bridge to get the adapter iOS dependency and make it available to a Flutter app.
+public class GmaMediationDTExchangePlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {}
+}
