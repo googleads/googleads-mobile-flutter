608cfe6e52d5824f679444e7cdaa011e4fbd7867
Aldo Becerril <aldobecerril@google.com>
2024-04-15 16:04:09 -0700

    Added iOS Privacy API

--- packages/mediation/gma_mediation_dtexchange/ios/Classes/GmaMediationDTExchangePlugin.swift
+++ packages/mediation/gma_mediation_dtexchange/ios/Classes/GmaMediationDTExchangePlugin.swift
@@ -1,7 +1,52 @@
 import Flutter
+import IASDKCore
 import UIKit
 
-/// Class that serves as bridge to get the adapter iOS dependency and make it available to a Flutter app.
-public class GmaMediationDTExchangePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {}
+/// Manages DTExchangePrivacyApi and implements the needed methods.
+public class GmaMediationDTExchangePlugin: NSObject, FlutterPlugin, DTExchangePrivacyApi {
+  let dtExchangeApi: DTExchangePrivacyProtocol
+
+  init (dtExchangeApi: DTExchangePrivacyProtocol) {
+    self.dtExchangeApi = dtExchangeApi
+  }
+
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let messenger : FlutterBinaryMessenger = registrar.messenger()
+    let api : DTExchangePrivacyApi&NSObjectProtocol=GmaMediationDTExchangePlugin.init(dtExchangeApi: DTExchangePrivacyImpl())
+    DTExchangePrivacyApiSetup.setUp(binaryMessenger: messenger, api: api)
+  }
+
+  func setGDPRConsent(gdprConsent: Bool) throws {
+    dtExchangeApi.setGDPRConsent(gdprConsent: gdprConsent)
+  }
+
+  func setGDPRConsentString(gdprConsentString: String) throws {
+    dtExchangeApi.setGDPRConsentString(gdprConsentString: gdprConsentString)
+  }
+
+  func setUSPrivacyString(usPrivacyString: String) throws {
+    dtExchangeApi.setUSPrivacyString(usPrivacyString: usPrivacyString)
+  }
+}
+
+protocol DTExchangePrivacyProtocol {
+  func setGDPRConsent(gdprConsent: Bool)
+
+  func setGDPRConsentString(gdprConsentString: String)
+
+  func setUSPrivacyString(usPrivacyString: String)
+}
+
+class DTExchangePrivacyImpl : DTExchangePrivacyProtocol {
+  func setGDPRConsent(gdprConsent: Bool) {
+    IASDKCore.sharedInstance().gdprConsent = gdprConsent ? IAGDPRConsentType.given : IAGDPRConsentType.denied
+  }
+  
+  func setGDPRConsentString(gdprConsentString: String) {
+    IASDKCore.sharedInstance().gdprConsentString = gdprConsentString
+  }
+  
+  func setUSPrivacyString(usPrivacyString: String) {
+    IASDKCore.sharedInstance().ccpaString = usPrivacyString
+  }
 }
