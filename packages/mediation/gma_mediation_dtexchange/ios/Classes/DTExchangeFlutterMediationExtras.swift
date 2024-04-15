f602b646e7660e90f271f87992ac980848dbd7e9
Aldo Becerril <aldobecerril@google.com>
2024-04-15 14:28:21 -0700

    Added DT Exchange iOS Mediation Extras

--- packages/mediation/gma_mediation_dtexchange/ios/Classes/DTExchangeFlutterMediationExtras.swift
+++ packages/mediation/gma_mediation_dtexchange/ios/Classes/DTExchangeFlutterMediationExtras.swift
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import Foundation
 import DTExchangeAdapter
 
@@ -15,9 +29,29 @@ class DTExchangeFlutterMediationExtras : NSObject, FLTMediationExtras {
     if let muteVideo = extras["muteVideo"] as? Bool {
       fyberExtras.muteAudio = muteVideo
     }
-//     if let userData = extras["userData"] as ? UserData {
-//       fyberExtras.userData = userData.asIAUserData()
-//     }
+    if (extras["age"] != nil || extras["gender"] != nil || extras["zipCode"] != nil) {
+        let userData = IAUserData.build({ builder in
+          if let age = self.extras["age"] as? UInt {
+            builder.age = age
+          }
+          if let gender = self.extras["gender"] as? UInt {
+            switch gender {
+            case 1:
+              builder.gender = IAUserGenderType.male
+            case 2:
+              builder.gender = IAUserGenderType.female
+            case 3:
+              builder.gender = IAUserGenderType.other
+            default:
+              builder.gender = IAUserGenderType.unknown
+            }
+          }
+          if let zipCode = self.extras["zipCode"] as? String {
+            builder.zipCode = zipCode
+          }
+        })
+        fyberExtras.userData = userData
+    }
     return fyberExtras
   }
 }
--- packages/mediation/gma_mediation_dtexchange/lib/dtexchange_mediation_extras.dart
+++ packages/mediation/gma_mediation_dtexchange/lib/dtexchange_mediation_extras.dart
@@ -1,5 +1,20 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+/// Class with additional parameters to attach to the [AdRequest] or [AdManagerAdRequest] when using the [GmaMediationDTExchange] adapter.
 class DTExchangeMediationExtras implements MediationExtras {
   DTExchangeMediationExtras({
     this.muteVideo,
@@ -10,7 +25,7 @@ class DTExchangeMediationExtras implements MediationExtras {
 
   bool? muteVideo;
   int? age;
-  int? gender;
+  DTEUserGenderType? gender;
   String? zipCode;
 
   @override
@@ -28,8 +43,16 @@ class DTExchangeMediationExtras implements MediationExtras {
     return <String, dynamic>{
       "muteVideo": muteVideo,
       "age": age,
-      "gender": gender,
+      "gender": gender?.intValue,
       "zipCode": zipCode,
     };
   }
+}
+
+enum DTEUserGenderType {
+  unknown,
+  male,
+  female,
+  other;
+  int get intValue => index;
 }
