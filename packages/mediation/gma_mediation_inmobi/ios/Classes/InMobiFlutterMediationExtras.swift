de9200a6e0573404ee51a77c6dfe984609821178
Aldo Becerril <aldobecerril@google.com>
2024-04-16 22:12:36 -0700

    Completed iOS Mediation Extras

--- packages/mediation/gma_mediation_inmobi/ios/Classes/InMobiFlutterMediationExtras.swift
+++ packages/mediation/gma_mediation_inmobi/ios/Classes/InMobiFlutterMediationExtras.swift
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import Foundation
 import InMobiAdapter
 
@@ -13,15 +27,53 @@ class InMobiFlutterMediationExtras : NSObject, FLTMediationExtras {
   func getMediationExtras() -> GADAdNetworkExtras {
     let inMobiExtras = GADInMobiExtras()
     if let ageGroup = extras["ageGroup"] as? Int {
-      inMobiExtras.ageGroup = ageGroup
+      switch ageGroup {
+      case 0:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.below18
+      case 1:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.between18And24
+      case 2:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.between25And29
+      case 3:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.between30And34
+      case 4:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.between35And44
+      case 5:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.between45And54
+      case 6:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.between55And65
+      case 7:
+        inMobiExtras.ageGroup = IMSDKAgeGroup.above65
+      default:
+        break;
+      }
     }
     if let educationType = extras["educationType"] as? Int {
-      inMobiExtras.educationType = educationType
+      switch educationType {
+      case 0:
+        inMobiExtras.educationType = IMSDKEducation.highSchoolOrLess
+      case 1:
+        inMobiExtras.educationType = IMSDKEducation.collageOrGraduate
+      case 2:
+        inMobiExtras.educationType = IMSDKEducation.postGraduateOrAbove
+      default:
+        break;
+      }
     }
     if let logLevel = extras["logLevel"] as? Int {
-      inMobiExtras.logLevel = logLevel
+      switch logLevel {
+      case 0:
+        inMobiExtras.logLevel = IMSDKLogLevel.none
+      case 1:
+        inMobiExtras.logLevel = IMSDKLogLevel.debug
+      case 2:
+        inMobiExtras.logLevel = IMSDKLogLevel.error
+      default:
+        break;
+      }
+
     }
-    if let age = extras["age"] as? Int {
+    if let age = extras["age"] as? UInt {
       inMobiExtras.age = age
     }
     if let yearOfBirth = extras["yearOfBirth"] as? Int {
@@ -42,8 +94,11 @@ class InMobiFlutterMediationExtras : NSObject, FLTMediationExtras {
     if let interests = extras["interests"] as? String {
       inMobiExtras.interests = interests
     }
-    if let city = extras["city"] as? String && let state = extras["state"] as? String && let areaCode = extras["country"] as? String {
-      inMobiExtras.setLocationWithCityStateCountry(city, state, country)
+    let city = extras["city"] as? String
+    let state = extras["state"] as? String
+    let country = extras["country"] as? String
+    if city != nil && state != nil && country != nil {
+      inMobiExtras.setLocationWithCity(city, state: state, country: country)
     }
 
     return inMobiExtras
