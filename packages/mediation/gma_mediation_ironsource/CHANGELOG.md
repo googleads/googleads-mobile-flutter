890b9b2be8bbb2dbceba8573500d3fed9a90c47f
Aldo Becerril <aldobecerril@google.com>
2024-05-09 15:59:01 -0700

    Implementation of IronSource Privacy API in the adapter (#1050)
    * IronSource Privacy APIs implementation
    
    * Updated ironSource GitHub workflow to run unit tests

--- packages/mediation/gma_mediation_ironsource/CHANGELOG.md
+++ packages/mediation/gma_mediation_ironsource/CHANGELOG.md
@@ -1,5 +1,7 @@
-## 1.0.0
+## ironSource Flutter Mediation Adapter Changelog
 
+#### 1.0.0
 * Initial release.
-* Verified compatibility with ironSource Android adapter version 7.9.0.0
-* Verified compatibility with ironSource iOS adapter version 7.9.1.0.0
+* Verified compatibility with ironSource Android adapter version 8.0.0.0
+* Verified compatibility with ironSource iOS adapter version 8.0.0.0.0
+* Built and tested with the Google Mobile Ads Flutter Plugin version 5.1.0.
--- packages/mediation/gma_mediation_ironsource/android/src/main/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_ironsource/GmaMediationIronsourcePlugin.kt
+++ packages/mediation/gma_mediation_ironsource/android/src/main/kotlin/io/flutter/plugins/googlemobileads/mediation/gma_mediation_ironsource/GmaMediationIronsourcePlugin.kt
@@ -14,14 +14,31 @@
 
 package io.flutter.plugins.googlemobileads.mediation.gma_mediation_ironsource
 
+import com.ironsource.mediationsdk.IronSource;
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
-/** GmaMediationIronsourcePlugin. */
-class GmaMediationIronsourcePlugin: FlutterPlugin {
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+/** GmaMediationIronsourcePlugin manages IronSourcePrivacyApi and implements the needed methods. */
+class GmaMediationIronsourcePlugin: FlutterPlugin, IronSourcePrivacyApi {
 
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    IronSourcePrivacyApi.setUp(flutterPluginBinding.binaryMessenger, this)
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    IronSourcePrivacyApi.setUp(binding.binaryMessenger, null)
+  }
+
+  override fun setConsent(gdprConsent: Boolean) {
+    IronSource.setConsent(gdprConsent)
+  }
+
+  override fun setDoNotSell(doNotSell: Boolean) {
+    IronSource.setMetaData(IRONSOURCE_DONOTSELL_KEY, if (doNotSell) TRUE else FALSE)
+  }
+
+  companion object {
+    const val IRONSOURCE_DONOTSELL_KEY = "do_not_sell"
+    const val TRUE = "true"
+    const val FALSE = "false"
   }
 }
