a1ef9f5ca2e560e3c01ddab2b20c297186571e97
Justin Malandruccolo <jmalandruccolo23@gmail.com>
2024-07-16 10:55:22 -0400

    Add Ad Inspector button to Banner Example (#1125)
    * Added ad inspector to app bar
    
    * removed changes
    
    * removed
    
    * new line
    
    * no changes
    
    * no changes pt 2
    
    * Updated minimum to iOS 12
    
    ---------
    
    Co-authored-by: Justin Malandruccolo <jmalandruccolo@google.com>

--- samples/admob/banner_example/lib/main.dart
+++ samples/admob/banner_example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import 'app_bar_item.dart';
 import 'consent_manager.dart';
 
 void main() {
@@ -12,7 +13,7 @@ void main() {
   ));
 }
 
-/// A simple app that loads a banner ad.
+/// An example app that loads a banner ad.
 class BannerExample extends StatefulWidget {
   const BannerExample({super.key});
 
@@ -21,10 +22,9 @@ class BannerExample extends StatefulWidget {
 }
 
 class BannerExampleState extends State<BannerExample> {
-  static const privacySettingsText = 'Privacy Settings';
-
   final _consentManager = ConsentManager();
   var _isMobileAdsInitializeCalled = false;
+  var _isPrivacyOptionsRequired = false;
   BannerAd? _bannerAd;
   bool _isLoaded = false;
   Orientation? _currentOrientation;
@@ -44,6 +44,9 @@ class BannerExampleState extends State<BannerExample> {
             "${consentGatheringError.errorCode}: ${consentGatheringError.message}");
       }
 
+      // Check if a privacy options entry point is required.
+      _getIsPrivacyOptionsRequired();
+
       // Attempt to initialize the Mobile Ads SDK.
       _initializeMobileAdsSDK();
     });
@@ -58,10 +61,7 @@ class BannerExampleState extends State<BannerExample> {
         title: 'Banner Example',
         home: Scaffold(
             appBar: AppBar(
-                title: const Text('Banner Example'),
-                actions: _isMobileAdsInitializeCalled
-                    ? _privacySettingsAppBarAction()
-                    : null),
+                title: const Text('Banner Example'), actions: _appBarActions()),
             body: OrientationBuilder(
               builder: (context, orientation) {
                 if (_currentOrientation != orientation) {
@@ -88,33 +88,36 @@ class BannerExampleState extends State<BannerExample> {
             )));
   }
 
-  List<Widget> _privacySettingsAppBarAction() {
+  List<Widget> _appBarActions() {
+    var array = [AppBarItem(AppBarItem.adInpsectorText, 0)];
+
+    if (_isPrivacyOptionsRequired) {
+      array.add(AppBarItem(AppBarItem.privacySettingsText, 1));
+    }
+
     return <Widget>[
-      // Regenerate the options menu to include a privacy setting.
-      FutureBuilder(
-          future: _consentManager.isPrivacyOptionsRequired(),
-          builder: (context, snapshot) {
-            final bool visibility = snapshot.data ?? false;
-            return Visibility(
-                visible: visibility,
-                child: PopupMenuButton<String>(
-                  onSelected: (String result) {
-                    if (result == privacySettingsText) {
-                      _consentManager.showPrivacyOptionsForm((formError) {
-                        if (formError != null) {
-                          debugPrint(
-                              "${formError.errorCode}: ${formError.message}");
-                        }
-                      });
-                    }
-                  },
-                  itemBuilder: (BuildContext context) =>
-                      <PopupMenuEntry<String>>[
-                    const PopupMenuItem<String>(
-                        value: privacySettingsText,
-                        child: Text(privacySettingsText))
-                  ],
-                ));
+      PopupMenuButton<AppBarItem>(
+          itemBuilder: (context) => array
+              .map((item) => PopupMenuItem<AppBarItem>(
+                    value: item,
+                    child: Text(
+                      item.label,
+                    ),
+                  ))
+              .toList(),
+          onSelected: (item) {
+            switch (item.value) {
+              case 0:
+                MobileAds.instance.openAdInspector((error) {
+                  // Error will be non-null if ad inspector closed due to an error.
+                });
+              case 1:
+                _consentManager.showPrivacyOptionsForm((formError) {
+                  if (formError != null) {
+                    debugPrint("${formError.errorCode}: ${formError.message}");
+                  }
+                });
+            }
           })
     ];
   }
@@ -169,6 +172,15 @@ class BannerExampleState extends State<BannerExample> {
     ).load();
   }
 
+  /// Redraw the app bar actions if a privacy options entry point is required.
+  void _getIsPrivacyOptionsRequired() async {
+    if (await _consentManager.isPrivacyOptionsRequired()) {
+      setState(() {
+        _isPrivacyOptionsRequired = true;
+      });
+    }
+  }
+
   /// Initialize the Mobile Ads SDK if the SDK has gathered consent aligned with
   /// the app's configured messages.
   void _initializeMobileAdsSDK() async {
@@ -176,14 +188,12 @@ class BannerExampleState extends State<BannerExample> {
       return;
     }
 
-    var canRequestAds = await _consentManager.canRequestAds();
-    if (canRequestAds) {
-      setState(() {
-        _isMobileAdsInitializeCalled = true;
-      });
+    if (await _consentManager.canRequestAds()) {
+      _isMobileAdsInitializeCalled = true;
 
       // Initialize the Mobile Ads SDK.
       MobileAds.instance.initialize();
+
       // Load an ad.
       _loadAd();
     }
