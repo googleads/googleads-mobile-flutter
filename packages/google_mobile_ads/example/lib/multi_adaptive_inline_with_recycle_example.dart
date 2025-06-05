1fe669f59ae38723256076e877b7c3ecdffe6105
Huan Lin <hellohuanlin@gmail.com>
2025-06-05 10:47:44 -0700

    address comments

--- packages/google_mobile_ads/example/lib/multi_adaptive_inline_with_recycle_example.dart
+++ packages/google_mobile_ads/example/lib/multi_adaptive_inline_with_recycle_example.dart
@@ -17,8 +17,10 @@ class _MultiInlineAdaptiveWithRecycleExampleState extends State<MultiInlineAdapt
   // Keep track of sizes of the banners (since they can be different sizes).
   final Map<BannerAd, AdSize> _bannerSizes = {};
 
-  // The maximum number of banners we create.
-  static const _cacheSize = 10;
+  // The maximum number of banners to create.
+  static const int _cacheSize = 10;
+  // Show a banner every 3 rows (i.e. row index 0, 3, 6, 9 etc will be banner rows.
+  static const int _adInterval = 3;
   // Keep track of the positions of banners.
   final Map<BannerAd, int> _bannerPositions = {};
 
@@ -35,7 +37,7 @@ class _MultiInlineAdaptiveWithRecycleExampleState extends State<MultiInlineAdapt
         onAdLoaded: (Ad ad) async {
           BannerAd bannerAd = (ad as BannerAd);
           final AdSize? adSize = await bannerAd.getPlatformAdSize();
-          // When the banner size is updated, we want to rebuild.
+          // When the banner size is updated, rebuild by calling setState.
           if (adSize != null && adSize != _bannerSizes[bannerAd]) {
             setState(() {
               _bannerSizes[bannerAd] = adSize;
@@ -79,25 +81,41 @@ class _MultiInlineAdaptiveWithRecycleExampleState extends State<MultiInlineAdapt
     return Scaffold(
       appBar: AppBar(title: Text('Adaptive Size, Recycle')),
       body: ListView.builder(
-        // Arbitrary example of 1000 items in the list.
-        itemCount: 1000,
+        // Arbitrary example of 100 items in the list.
+        itemCount: 100,
         itemBuilder: (BuildContext context, int index) {
-          // We show a banner every 3 rows (i.e. row index 0, 3, 6, 9, 12, etc will be banner row)
-          if (index % 3 == 0) {
-            int bannerPosition = index ~/ 3;
+          if (index % _adInterval == 0) {
+            int bannerPosition = index ~/ _adInterval;
             BannerAd bannerAd = _getRecycledBannerAd(bannerPosition);
             final AdSize? adSize = _bannerSizes[bannerAd];
             if (adSize == null) {
               // Null adSize means the banner's content is not fetched yet.
-              return SizedBox(height: 50, child: Text('banner is loading'));
+              return SizedBox.shrink();
             }
             // Now this banner is loaded with ad content and corresponding ad size.
             return SizedBox(width: adSize.width.toDouble(), height: adSize.height.toDouble(), child: AdWidget(ad: bannerAd));
           }
 
           // Show your regular non-ad content.
-          return SizedBox(height: 200, child: ColoredBox(color: Colors.yellow));
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 1.0,
+                style: BorderStyle.solid,
+              ),
+            ),
+            child: SizedBox(height: 100, child: ColoredBox(color: Colors.yellow)),
+          );
         }),
     );
+  }
+
+  @override
+  void dispose() {
+    for (final banner in _banners) {
+      banner.dispose();
+    }
+    super.dispose();
   }
 }
