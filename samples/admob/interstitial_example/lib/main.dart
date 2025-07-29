332b6a287f181874c5253109cb314f6e2b3786d2
Justin Malandruccolo <jmalandruccolo@google.com>
2025-07-29 10:13:30 -0400

    formatted

--- samples/admob/interstitial_example/lib/main.dart
+++ samples/admob/interstitial_example/lib/main.dart
@@ -197,7 +197,8 @@ class InterstitialExampleState extends State<InterstitialExample> {
             },
             onAdFailedToShowFullScreenContent: (ad, err) {
               // Called when the ad failed to show full screen content.
-              debugPrint('Ad failed to show full screen content with error: $err');
+              debugPrint(
+                  'Ad failed to show full screen content with error: $err');
               // Dispose the ad here to free resources.
               ad.dispose();
             },
