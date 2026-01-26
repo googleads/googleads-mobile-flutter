dd1a53b18e64f2bfd987d15b5584542e58bfc002
Stuart Morgan <stuartmorgan@google.com>
2026-01-26 14:45:26 -0500

    Add the webview_flutter_wkwebview dependency in SwiftPM

--- packages/google_mobile_ads/ios/google_mobile_ads/Package.swift
+++ packages/google_mobile_ads/ios/google_mobile_ads/Package.swift
@@ -6,20 +6,22 @@ import PackageDescription
 let package = Package(
     name: "google_mobile_ads",
     platforms: [
-        .iOS("12.0")
+        .iOS("13.0")
     ],
     products: [
         .library(name: "google-mobile-ads", targets: ["google_mobile_ads"])
     ],
     dependencies: [
         .package(
-            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", from: "12.14.0")
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", from: "12.14.0"),
+        .package(name: "webview_flutter_wkwebview", path: "../webview_flutter_wkwebview"),
     ],
     targets: [
         .target(
             name: "google_mobile_ads",
             dependencies: [
-              .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+              .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+              .product(name: "webview-flutter-wkwebview", package: "webview_flutter_wkwebview"),
             ],
             resources: [
               .process("Resources"),
