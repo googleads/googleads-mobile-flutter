37c8946b46ba85d50d5cd0a5681069fe25028ed6
Justin Malandruccolo <jmalandruccolo@google.com>
2026-08-18 18:16:17 -0400

    Move includecode snippets from samples to snippets

--- packages/google_mobile_ads/example/lib/snippets/banner_ad_snippets.dart
+++ packages/google_mobile_ads/example/lib/snippets/banner_ad_snippets.dart
@@ -35,8 +35,74 @@ class _BannerAdSnippets extends State<_BannerAdWidget> {
       : 'ca-app-pub-3940256099942544/2435281174';
   final String _adManagerAdUnitId = '/21775744923/example/adaptive-banner';
 
-  // [START load_ad_ad_manager]
+  // [START load_ad]
   void _loadAd() async {
+    // [START get_ad_size]
+    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
+    final size = await AdSize.getLargeAnchoredAdaptiveBannerAdSize(
+      MediaQuery.sizeOf(context).width.truncate(),
+    );
+    // [END get_ad_size]
+
+    if (size == null) {
+      // Unable to get width of anchored banner.
+      return;
+    }
+
+    unawaited(
+      BannerAd(
+        adUnitId: _adUnitId,
+        request: const AdRequest(),
+        size: size,
+        listener: BannerAdListener(
+          onAdLoaded: (ad) {
+            // Called when an ad is successfully received.
+            debugPrint('Ad was loaded.');
+            setState(() {
+              _bannerAd = ad as BannerAd;
+            });
+          },
+          onAdFailedToLoad: (ad, err) {
+            // Called when an ad request failed.
+            debugPrint('Ad failed to load with error: $err');
+            ad.dispose();
+          },
+          // [START_EXCLUDE silent]
+          // [START ad_events]
+          onAdOpened: (Ad ad) {
+            // Called when an ad opens an overlay that covers the screen.
+            debugPrint('Ad was opened.');
+          },
+          onAdClosed: (Ad ad) {
+            // Called when an ad removes an overlay that covers the screen.
+            debugPrint('Ad was closed.');
+          },
+          onAdImpression: (Ad ad) {
+            // Called when an impression occurs on the ad.
+            debugPrint('Ad recorded an impression.');
+          },
+          onAdClicked: (Ad ad) {
+            // Called when a click event occurs on the ad.
+            debugPrint('Ad was clicked.');
+          },
+          onAdWillDismissScreen: (Ad ad) {
+            // iOS only. Called before dismissing a full screen view.
+            debugPrint('Ad will be dismissed.');
+          },
+          // [END ad_events]
+          // [END_EXCLUDE]
+        ),
+      ).load(),
+    );
+  }
+  // [END load_ad]
+
+  // ===================================================================
+  // Ad Manager snippets
+  // ===================================================================
+
+  // [START load_ad_ad_manager]
+  void _loadAdManagerBannerAd() async {
     // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
     final size = await AdSize.getLargeAnchoredAdaptiveBannerAdSize(
       MediaQuery.sizeOf(context).width.truncate(),
@@ -73,7 +139,28 @@ class _BannerAdSnippets extends State<_BannerAdWidget> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Stack(
+      children: [
+        // [START display_ad]
+        if (_bannerAd != null)
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SafeArea(
+              child: SizedBox(
+                width: _bannerAd!.size.width.toDouble(),
+                height: _bannerAd!.size.height.toDouble(),
+                child: AdWidget(ad: _bannerAd!),
+              ),
+            ),
+          ),
+        // [END display_ad]
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
   }
 }
