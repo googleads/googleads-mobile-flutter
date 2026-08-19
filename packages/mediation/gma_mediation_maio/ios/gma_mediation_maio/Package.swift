431116e8cd2204da4d4f172e944a53794e9125b8
Aldo Becerril <aldobecerril@google.com>
2026-08-19 11:28:02 -0700

    Migrating maio plugin adapter to SPM
    PiperOrigin-RevId: 967316921

---
+++ packages/mediation/gma_mediation_maio/ios/gma_mediation_maio/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "gma_mediation_maio",
+  platforms: [
+    .iOS("13.0")
+  ],
+  products: [
+    .library(
+      name: "gma-mediation-maio",
+      targets: ["gma_mediation_maio"]
+    )
+  ],
+  dependencies: [
+    .package(name: "FlutterFramework", path: "../FlutterFramework"),
+    .package(
+      url: "https://github.com/googleads/googleads-mobile-ios-mediation-maio.git",
+      from: "2.2.102"),
+  ],
+  targets: [
+    .target(
+      name: "gma_mediation_maio",
+      dependencies: [
+        .product(name: "FlutterFramework", package: "FlutterFramework"),
+        .product(
+          name: "MaioAdapterTarget",
+          package: "googleads-mobile-ios-mediation-maio"),
+      ],
+      path: "Sources/gma_mediation_maio"
+    )
+  ]
+)
